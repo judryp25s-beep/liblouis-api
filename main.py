8ec594e835d569e91fa11a98ccb0ac3f1141af0f
MANDIMBISOA Judry <judry.p25s@gmail.com>
2025-12-31 16:42:11 +0300

    Create main.py
    Main files, for the routes (using Python-Flask)

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, send_file
+import louis
+import io
+
+app = Flask(__name__)
+
+@app.route("/brf", methods=["POST"])
+def brf():
+    texte = request.json.get("texte", "")
+
+    table = "fr-bfu-g1.ctb"
+    braille = louis.translateString(table, texte)
+
+    # créer un fichier en mémoire
+    fichier = io.BytesIO()
+    fichier.write(braille.encode("ascii", errors="ignore"))
+    fichier.seek(0)
+
+    return send_file(
+        fichier,
+        mimetype="text/plain",
+        as_attachment=True,
+        download_name="texte.brf"
+    )
+
+app.run(host="0.0.0.0", port=10000)
